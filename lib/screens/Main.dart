c530e08e4a55f8a701946b66a6f07d91ec97750b
Gleb Brodskiy <h.brodskyi@newlinetechnologies.net>
2021-09-20 16:22:18 +0300

    - Added Inherited Widget to change a name color. - Added PProvider to change a "favorite" color

--- lib/screens/Main.dart
+++ lib/screens/Main.dart
@@ -458,3 +458,22 @@ Widget _buildPopularMenu(int index){
 }
 }
 
+class Constant extends InheritedWidget {
+  final Color neededColor;
+  const Constant({
+    required this.neededColor,
+    required Widget child
+  }) : super(child: child);
+
+
+
+  @override
+  bool updateShouldNotify(Constant old) => neededColor != old.neededColor;
+
+  static Constant? of(BuildContext context){
+     final Constant? result = context.dependOnInheritedWidgetOfExactType<Constant>();
+     assert(result != null, 'No Constant found in context');
+     return result!;
+  }
+
+}
